2c97ff88cc11ee16caa9dab8bcbcb200e780579c
Castrense Savojardo <castrense.savojardo2@unibo.it>
2020-12-03 17:13:56 +0100

    Bugfix

--- bacellolib/config.py
+++ bacellolib/config.py
@@ -1,5 +1,5 @@
 locmap = {"Cytoplasm": ("Cytoplasm", "GO:0005737"),
           "Nucleus": ("Nucleus", "GO:0005634"),
-          "Secreted": ("Extracellular", "GO:0005615"),
+          "Secretory": ("Extracellular", "GO:0005615"),
           "Mitochondrion": ("Mitochondrion", "GO:0005739"),
           "Chloroplast": ("Chloroplast", "GO:0009507")}
